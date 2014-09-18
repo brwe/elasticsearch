9e0b7deb7c79494099d9094f1550d72f987c1609
Britta Weber <britta.weber@elasticsearch.com>
2014-09-18 09:26:45 +0200

    test for agg on parent id

--- src/test/java/org/elasticsearch/search/aggregations/bucket/DedicatedAggregationTests.java
+++ src/test/java/org/elasticsearch/search/aggregations/bucket/DedicatedAggregationTests.java
@@ -19,15 +19,21 @@
 package org.elasticsearch.search.aggregations.bucket;
 
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.filter.Filter;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import static org.elasticsearch.common.io.Streams.copyToStringFromClasspath;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertSearchResponse;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -52,5 +58,27 @@ public class DedicatedAggregationTests extends ElasticsearchIntegrationTest {
         Filter filterAgg = (Filter) searchResponse.getAggregations().getAsMap().get("issue7240");
         assertThat(filterAgg.getAggregations().getAsMap().get("terms"), instanceOf(StringTerms.class));
         assertThat(((StringTerms) filterAgg.getAggregations().getAsMap().get("terms")).getBuckets().get(0).getDocCount(), equalTo(1l));
+    }
+
+    @Test
+    public void testParentIdAggregation() throws IOException {
+        XContentBuilder mapping = jsonBuilder().startObject()
+                .startObject("childtype")
+                .startObject("_parent")
+                .field("type", "parenttype")
+                .field("store", true)
+                .endObject()
+                .endObject();
+        assertAcked(prepareCreate("testidx").addMapping("childtype", mapping));
+        client().prepareIndex("testidx", "childtype").setSource(jsonBuilder().startObject().field("num", 1).endObject()).setParent("p1").get();
+        client().prepareIndex("testidx", "childtype").setSource(jsonBuilder().startObject().field("num", 2).endObject()).setParent("p1").get();
+
+        refresh();
+        ensureGreen("testidx");
+        SearchResponse searchResponse = client().prepareSearch("testidx").setTypes("childtype").setQuery(matchAllQuery()).addAggregation(AggregationBuilders.terms("children").field("_parent")).get();
+        assertThat(searchResponse.getHits().getTotalHits(), equalTo(2l));
+        assertSearchResponse(searchResponse);
+        assertThat(searchResponse.getAggregations().getAsMap().get("children"), instanceOf(Terms.class));
+        Terms terms = (Terms) searchResponse.getAggregations().getAsMap().get("children");
     }
 }
