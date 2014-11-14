7596d4fb6f423a1a2ba6f25a6586ee8a6ede8201
Britta Weber <britta.weber@elasticsearch.com>
2014-11-14 20:36:59 +0100

    test

--- src/test/java/org/elasticsearch/search/aggregations/bucket/DedicatedAggregationTests.java
+++ src/test/java/org/elasticsearch/search/aggregations/bucket/DedicatedAggregationTests.java
@@ -18,16 +18,26 @@
  */
 package org.elasticsearch.search.aggregations.bucket;
 
+import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.search.aggregations.bucket.filter.Filter;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.elasticsearch.common.io.Streams.copyToStringFromClasspath;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
+import static org.elasticsearch.search.aggregations.AggregationBuilders.*;
+import static org.elasticsearch.search.aggregations.AggregationBuilders.avg;
+import static org.elasticsearch.search.aggregations.AggregationBuilders.histogram;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertSearchResponse;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -52,5 +62,39 @@ public class DedicatedAggregationTests extends ElasticsearchIntegrationTest {
         Filter filterAgg = (Filter) searchResponse.getAggregations().getAsMap().get("issue7240");
         assertThat(filterAgg.getAggregations().getAsMap().get("terms"), instanceOf(StringTerms.class));
         assertThat(((StringTerms) filterAgg.getAggregations().getAsMap().get("terms")).getBuckets().get(0).getDocCount(), equalTo(1l));
+    }
+
+    @Test
+    public void testNaiveCorrelationWithFilterBucketUnit() throws IOException, ExecutionException, InterruptedException {
+        indexDocs("label");
+        SearchResponse searchResponse = client().prepareSearch("index").setQuery(matchAllQuery())
+                .addAggregation(terms("dummy_agg").field("dummy_field")
+                        .subAggregation(terms("label").field("label")
+                                .subAggregation(histogram("x").field("x").interval(1)
+                                        .subAggregation(avg("avg").field("y"))))
+                        .subAggregation(filter("reference_label").filter(FilterBuilders.termFilter("label", "reference"))
+                                .subAggregation(histogram("x").field("x").interval(1)
+                                        .subAggregation(avg("avg").field("y")))))
+
+                .get();
+        //searchResponse.getAggregations().get("dummy_agg").getProperty(Arrays.asList("label.x.avg".split("\\.")));
+        searchResponse.getAggregations().get("dummy_agg").getProperty("reference_label>x>avg");
+
+    }
+
+
+    private void indexDocs(String referenceLabelField) throws IOException, ExecutionException, InterruptedException {
+        List<IndexRequestBuilder> indexRequests = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 10; j++) {
+                int value = i == 1 ? 10 - j : j;
+                indexRequests.add(client().prepareIndex().setSource(jsonBuilder().startObject().field("dummy_field", "dummy_value").field("label", Integer.toString(i)).field("y", value).field("x", j).endObject()).setIndex("index").setType("type"));
+            }
+        }
+        for (int j = 0; j < 10; j++) {
+            indexRequests.add(client().prepareIndex().setSource(jsonBuilder().startObject().field("dummy_field", "dummy_value").field("x", j).field(referenceLabelField, "reference").field("y", 9 - j).endObject()).setIndex("index").setType("type"));
+        }
+        indexRandom(true, indexRequests);
+        ensureGreen("index");
     }
 }
